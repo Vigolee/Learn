57037854dd5adfe1ba6d39000faef1736dc6de97
VigoLee <87175897@qq.com>
2016-05-31 16:59:54 +0800

    add a countDownLatchDemo

---
+++ src/main/java/learn/java/concurrent/tools/CountDownLatchDemo.java
@@ -0,0 +1,34 @@
+package learn.java.concurrent.tools;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 并发工具类
+ * Created by Vigo on 16/5/31.
+ */
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        new Thread(new ThreadDemo(countDownLatch)).start();
+        countDownLatch.await(1000, TimeUnit.MILLISECONDS);// 主线程等待
+        System.out.println(3);
+    }
+
+    static class ThreadDemo implements Runnable {
+
+        private CountDownLatch countDownLatch;
+
+        public ThreadDemo(CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+        }
+
+        public void run() {
+            System.out.println(1);
+            countDownLatch.countDown();
+            System.out.println(2);
+            //countDownLatch.countDown();
+        }
+    }
+}
