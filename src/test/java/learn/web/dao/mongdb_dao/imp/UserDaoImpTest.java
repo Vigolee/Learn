23f681117b8ed81303f84512a1cdd79c77fbb623
VigoLee <87175897@qq.com>
2016-08-25 13:36:37 +0800

    add some tests

--- src/test/java/learn/web/dao/mongdb_dao/imp/UserDaoImpTest.java
+++ src/test/java/learn/web/dao/mongdb_dao/imp/UserDaoImpTest.java
@@ -1,28 +1,55 @@
 package learn.web.dao.mongdb_dao.imp;
 
+import learn.web.config.BasicTest;
 import learn.web.dao.mongodb_dao.imp.UserDaoImp;
+import learn.web.po.mongdb_model.User;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * Created by Vigo on 16/8/23.
  */
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:conf/applicationContext.xml",
-        "classpath:conf/SpringMVC-servlet.xml"})
-public class UserDaoImpTest {
+public class UserDaoImpTest extends BasicTest{
 
     @Resource
     UserDaoImp userDaoImp;
 
     @Test
-         public void _testTest() {
+    public void _testTest() {
         userDaoImp._test();
+    }
+
+    @Test
+    public void insertTest(){
+        User user = new User();
+        user.setId("5");
+        user.setAge(1);
+        user.setBirth(new Date());
+        user.setPassword("123456");
+        user.setRegionName("北京");
+        user.setWorks(1);
+        userDaoImp.insert(user);
+    }
+
+    @Test
+    public void findOneTest(){
+        User user = userDaoImp.findOne("5");
+        System.out.println(user);
+    }
+
+    @Test
+    public void updateTest(){
+        User user = new User();
+        user.setId("5");
+        user.setAge(1);
+        user.setBirth(new Date());
+        user.setPassword("12123213");
+        user.setRegionName("New York");
+        user.setWorks(1);
+        userDaoImp.update(user);
     }
 }
