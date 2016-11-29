a3dfa71e21d6b730cfe7d3071313df997fbe8b70
Vigo <vigo.lee@yahoo.com>
2016-11-29 22:03:23 +0800

    add new field

--- src/main/java/learn/mq/conf/Configuration.java
+++ src/main/java/learn/mq/conf/Configuration.java
@@ -8,6 +8,12 @@ public class Configuration {
     // 队列名称
     public final static  String QUEUE_NAME = "MyQueue";
 
+    // 队列名称
+    public final static  String WORK_QUEUE_NAME = "WorkQueue";
+
     // 本地MQ连接地址
     public final static  String LOCAL_HOST = "localhost";
+
+    // 转发器名称
+    public static final String EXCHANGE_NAME = "log";
  }
