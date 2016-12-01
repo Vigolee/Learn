2dfccab3df94cc17820c15b1c4fc74fefda574b2
Vigo <vigo.lee@yahoo.com>
2016-12-01 21:21:35 +0800

    configure RabbitMQ Producer in spring

---
+++ src/main/java/learn/web/mq/ProduceMessage.java
@@ -0,0 +1,19 @@
+package learn.web.mq;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 生产消息
+ * Created by Vigo on 16/12/1.
+ */
+@Component("produceMessage")
+public class ProduceMessage {
+    @Autowired
+    private AmqpTemplate template;
+
+    public void emitMessage(String key, String msg){
+        template.convertAndSend(key, msg);
+    }
+}
